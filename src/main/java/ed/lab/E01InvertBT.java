8b8abcb62a0b65b589b8fa1ba7e21b84d6fc98b8
Gerardo Avila <gjavila@correo.url.edu.gt>
2025-03-23 21:43:18 -0600

    E01 Invert

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -2,6 +2,15 @@ package ed.lab;
 
 public class E01InvertBT {
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
+        if (root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
 }
