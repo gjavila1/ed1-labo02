3eda4ba9680ad6b5dcf9b324c0ab8b6954ac6011
Gerardo Avila <gjavila@correo.url.edu.gt>
2025-03-24 23:26:37 -0600

    E02 Invert (primer)

--- src/main/java/ed/lab/E02KthSmallest.java
+++ src/main/java/ed/lab/E02KthSmallest.java
@@ -1,9 +1,22 @@
 package ed.lab;
+import java.util.LinkedList;
+import java.util.List;
 
 public class E02KthSmallest {
+    LinkedList<Integer> result = new LinkedList<>();
 
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        traverse(root, result); // Traverse inorder to save all nodes in a LinkedList
+        return result.get(k - 1); // Return node in k-1 position;
     }
 
+    // Recursive traverse inorder code
+    private void traverse(TreeNode<Integer> root, List<Integer> result) {
+        if (root == null)
+            return;
+
+        traverse(root.left, result);
+        result.add(root.value);
+        traverse(root.right, result);
+    }
 }
